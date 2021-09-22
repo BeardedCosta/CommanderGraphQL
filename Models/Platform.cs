b2f8862de5f4285095b5090a8ef9cb0dd8182402
Costa88 <tiago.simoes.costa@pt.softinsa.com>
2021-09-22 21:24:51 +0100

    Pushing to repository

---
+++ Models/Platform.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using HotChocolate;
+
+namespace CommanderGQL.Models
+{
+    public class Platform
+    {
+        /// <summary>
+        /// Represents the unique ID for the platform.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Represents the name for the platform.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Represents a purchased, valid license for the platform.
+        /// </summary>
+        public string LicenseKey { get; set; }
+
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+
+    }
+}
